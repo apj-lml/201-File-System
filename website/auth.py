753f8b6eb86a3a5dc57343e62001dd540c22eec3
Aljohn Jacinto <aljohnjacinto.aj@gmail.com>
2021-11-22 16:53:38 +0800

    commit as of 11/22/2021

--- website/auth.py
+++ website/auth.py
@@ -20,6 +20,8 @@ def page_not_found(e):
 
 @auth.route('/', methods=['GET', 'POST'])
 def login():
+	logout_user()
+	session.clear()
 	if request.method == 'POST':
 		formdata = json.loads(request.data)
 		email = formdata['email']
@@ -29,7 +31,8 @@ def login():
 		if user:
 			if check_password_hash(user.password, password):
 				flash('Login successful', category = 'success')
-				login_user(user, remember = True)
+				login_user(user)
+				#session.permanent = False
 				if user.type_of_user == 'admin':
 					identity_changed.send(current_app._get_current_object(),
                                   identity=Identity(user.id))
@@ -40,11 +43,12 @@ def login():
 					return redirect(url_for('views.dashboard'))
 			else:
 				flash('Incorrect password, try again', category='error')
+				session['error_msg'] = 'Incorrect username or password!'
+				return jsonify(session['error_msg'])
 		else:
-			flash('Email does not exist', category='error')
+			session['error_msg'] = 'Email does not exist!'
+			return jsonify(session['error_msg'])
 	return render_template('login.html', user=current_user)
-
-
 
 
 @auth.route('/logout', methods=['GET', 'POST'])
@@ -62,89 +66,87 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+@auth.route('/check-token', methods=['GET', 'POST'])
+def check_token():
 
-
+	return render_template('check_token.html')
 
 @auth.route('/signup', methods=['GET', 'POST'])
 def signup():
+	if 'token-verified' in session:
+		if request.method == 'POST':
+			logout_user()
+			#formdata = json.loads(request.data) <-- use this if receiving json request
+			formdata = request.form.to_dict()
 
-	if request.method == 'POST':
+			email = formdata['email']
+			password1 = formdata['password']
+			password2 = formdata['floatingPassword2']
 
-		#formdata = json.loads(request.data) <-- use this if receiving json request
-		formdata = request.form.to_dict()
+			user = User.query.filter_by(email=email).first()
+			if user:
+				flash('User already exists', category = 'error')
+				return 'Duplicate User', 200
+			elif password1 != password2:
+				flash('Password does not match', category = 'error')
+				return 'Password Error', 200
 
-
-		email = formdata['email']
-		password1 = formdata['password']
-		password2 = formdata['floatingPassword2']
-
-		user = User.query.filter_by(email=email).first()
-		if user:
-			flash('User already exists', category = 'error')
-			return 'Duplicate User', 200
-		elif password1 != password2:
-			flash('Password does not match', category = 'error')
-			return 'Password Error', 200
-
-		else:
-			formdata['password'] = generate_password_hash(password1)
-			if formdata['date_of_validity'] != '':
-				formdata['date_of_validity'] = datetime.strptime(formdata['date_of_validity'], '%Y-%m-%d').date()
 			else:
-				formdata['date_of_validity'] = None
-			formdata['birthdate'] = datetime.strptime(formdata['birthdate'], '%Y-%m-%d').date()
-
-#----------------popping unnecessary data before saving to database------------
-			formdata.pop('floatingPassword2')
-			formdata.pop('same_as_permanent')
-#--------------------------------end of popping------------------------------
-
-			# conv = lambda i:i or None
-			# converteddata = [conv(i) for i in formdata]
-			# print("The list after conversion of Empty Strings : " + str(converteddata))
-			
-			finaldata = User(**formdata)
-			db.session.add(finaldata)
-			db.session.flush()
-			db.session.commit()
-
-#------------------this is for file upload--------------
-			final_name = ''
-			for afile in request.files:
-				file = request.files[afile]
-
-				print(f'print file: {afile}')
-				if afile not in request.files:
-					print('No file selected part')
-					return redirect(request.url)
-
-				if not file and allowed_file(file.filename):
-					print('Invalid file submitted')
-					return redirect(request.url)
+				formdata['password'] = generate_password_hash(password1)
+				if formdata['date_of_validity'] != '':
+					formdata['date_of_validity'] = datetime.strptime(formdata['date_of_validity'], '%Y-%m-%d').date()
 				else:
-					file_extension = file.filename.rsplit('.', 1)[1].lower()
-					file_name = file.filename.rsplit('.', 1)[0]
-					final_name = secure_filename(afile+'_'+ formdata['last_name']+'_'+formdata['first_name'] + '_' + formdata['employee_id'] +'_' + file_name +'.'+file_extension)
-					if os.path.isfile(current_app.config['UPLOAD_FOLDER']):
-						print('path does not exist... creating path')
-						os.mkdir(current_app.config['UPLOAD_FOLDER'])
+					formdata['date_of_validity'] = None
+				formdata['birthdate'] = datetime.strptime(formdata['birthdate'], '%Y-%m-%d').date()
+
+	#----------------popping unnecessary data before saving to database------------
+				formdata.pop('floatingPassword2')
+				formdata.pop('same_as_permanent')
+	#--------------------------------end of popping------------------------------
+
+				# conv = lambda i:i or None
+				# converteddata = [conv(i) for i in formdata]
+				# print("The list after conversion of Empty Strings : " + str(converteddata))
+				
+				finaldata = User(**formdata)
+				db.session.add(finaldata)
+				db.session.flush()
+				db.session.commit()
+
+	#------------------this is for file upload--------------
+				final_name = ''
+				for afile in request.files:
+					file = request.files[afile]
+
+					print(f'print file: {afile}')
+					if afile not in request.files:
+						print('No file selected part')
+						return redirect(request.url)
+
+					if not file and allowed_file(file.filename):
+						print('Invalid file submitted')
+						return redirect(request.url)
 					else:
-						print('path exist!')
-						file.save(os.path.join(current_app.config['UPLOAD_FOLDER'], final_name))
+						file_extension = file.filename.rsplit('.', 1)[1].lower()
+						file_name = file.filename.rsplit('.', 1)[0]
+						final_name = secure_filename(afile+'_'+ formdata['last_name']+'_'+formdata['first_name'] + '_' + formdata['employee_id'] +'_' + file_name +'.'+file_extension)
+						if os.path.isfile(current_app.config['UPLOAD_FOLDER']):
+							print('path does not exist... creating path')
+							os.mkdir(current_app.config['UPLOAD_FOLDER'])
+						else:
+							print('path exist!')
+							file.save(os.path.join(current_app.config['UPLOAD_FOLDER'], final_name))
 
-						#saving upload info to database
-						files_to_upload = Uploaded_File(file_name = final_name, file_path = '\\static\\files\\' + final_name, file_tag = afile, user_id = finaldata.id)
-						db.session.add(files_to_upload)
-						db.session.commit()
-#------------------end of file upload--------------------
-
-			return 'ok'
-	return render_template('signup.html', user=current_user)
-
-
-
-
-
+							#saving upload info to database
+							files_to_upload = Uploaded_File(file_name = final_name, file_path = '\\static\\files\\' + final_name, file_tag = afile, user_id = finaldata.id)
+							db.session.add(files_to_upload)
+							db.session.commit()
+	#------------------end of file upload--------------------
+		else:
+			return render_template('signup.html', user=current_user)
+		
+	return redirect(url_for('auth.login'))
+	
 
 @auth.route('/home')
 def home():
