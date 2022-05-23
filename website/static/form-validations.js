646e06e5141d1a031f62160806934efebbeae5f2
Aljohn Jacinto <aljohnjacinto.aj@gmail.com>
2022-05-23 16:19:19 +0800

    hotfix on WES casual step

--- website/static/form-validations.js
+++ website/static/form-validations.js
@@ -191,7 +191,7 @@
           sg.disabled = false;
           //sg.value = "1";
           job_grade.disabled = true;
-          //job_grade.value = "";
+          job_grade.value = "";
           // disableSgJobOrder(true);
 
 
