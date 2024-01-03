f0681c278936ad67848a2db9a67df26eab556e68
Aljohn Jacinto <aljohnjacinto.aj@gmail.com>
2024-01-03 17:00:31 +0800

    asdasd

--- website/saln.py
+++ website/saln.py
@@ -298,10 +298,10 @@ def get_context(id, filing_date, filing_type):
     user_profile_dict["children_list"] = sorted(children_list, key=lambda x: x.childAge, reverse=True)
 
     user_profile_dict['total_rp_acquisition_cost_p1'] = formatNumber(getRpAcquisitionCostSubTotal(user, 0, 3) + getRpAcquisitionCostSubTotal(user, 3, 7)) if getRpAcquisitionCostSubTotal(user, 0, 3) != 0 else formatNumber(0.00)
-    user_profile_dict['total_rp_acquisition_cost_p2'] = formatNumber(getRpAcquisitionCostSubTotal(user, 3, 7)) if getRpAcquisitionCostSubTotal(user, 3, 7) != 0 else 0.00
+    user_profile_dict['total_rp_acquisition_cost_p2'] = formatNumber(getRpAcquisitionCostSubTotal(user, 3, 7)) if getRpAcquisitionCostSubTotal(user, 3, 7) != 0 else formatNumber(0.00)
 
     user_profile_dict['total_pp_acquisition_cost_p1'] = formatNumber(getPpAcquisitionCostSubTotal(user, 0, 6) + getPpAcquisitionCostSubTotal(user, 6, 12)) if getPpAcquisitionCostSubTotal(user, 0, 6) != 0 else formatNumber(0.00)
-    user_profile_dict['total_pp_acquisition_cost_p2'] = formatNumber(getPpAcquisitionCostSubTotal(user, 6, 12)) if getPpAcquisitionCostSubTotal(user, 6, 12) != 0 else 0.00
+    user_profile_dict['total_pp_acquisition_cost_p2'] = formatNumber(getPpAcquisitionCostSubTotal(user, 6, 12)) if getPpAcquisitionCostSubTotal(user, 6, 12) != 0 else formatNumber(0.00)
 
     user_profile_dict['total_liability_outstanding_balance_p1'] = formatNumber(getLiabilityOutstandingBalance(user, 0, 3) + getLiabilityOutstandingBalance(user, 3, 8)) if getLiabilityOutstandingBalance(user, 0, 3) != 0 else formatNumber(0.00)
     user_profile_dict['total_liability_outstanding_balance_p2'] = formatNumber(getLiabilityOutstandingBalance(user, 3, 8)) if getLiabilityOutstandingBalance(user, 3, 8) != 0 else formatNumber(0.00)
